475c265b29b2e4d0e7aff65352f9ff117a57811c
luckyfoot <solorio13@gmail.com>
2016-11-22 17:31:00 -0800

    Newest update
    This is the new code that was posted to ada

--- find_author.py
+++ find_author.py
@@ -1,6 +1,5 @@
 import os.path, math
-
-# Test comment for commit. - RD
+import re
 
 def clean_up(s):
     ''' Return a version of string s in which all letters have been
@@ -42,18 +41,17 @@ def hapax_legomana_ratio(text):
     # To do: Replace this function's body to meet its specification.
     return 1
 
+'''
+You don't need this function.
+you can use re.split(), where you give it a regular expression.
+Regular expressions include '[!\?\.]'
 
 def split_on_separators(original, separators):
-    ''' Return a list of non-empty, non-blank strings from the original string
+     Return a list of non-empty, non-blank strings from the original string
     determined by splitting the string on any of the separators.
-    separators is a string of single-character separators.'''
-    
-    # To do: Complete this function's body to meet its specification.
-    # You are not required to keep the two lines below but you may find
-    # them helpful. (Hint)
-    result = [original]
-    return result
-                
+    separators is a string of single-character separators.
+
+'''   
     
 def average_sentence_length(text):
     ''' Return the average number of words per sentence in text.
